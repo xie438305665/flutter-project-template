7fe3a9e5172e9f6108ce983e47d41609cfc8dd53
Android_Xcl <438305665@qq.com>
2020-04-28 18:57:25 +0800

    login

--- lib/widgets/pages/login_page.dart
+++ lib/widgets/pages/login_page.dart
@@ -3,8 +3,6 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:zsy/routes/app_navigator.dart';
-import 'package:zsy/routes/app_route.dart';
 
 ///@description:登录
 ///@author xcl qq:244672784
@@ -17,6 +15,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String _userName;
   String _password;
+
+  bool _isSuccess;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSuccess = true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,66 +44,77 @@ class _LoginPageState extends State<LoginPage> {
                       padding: EdgeInsets.all(20),
                       child: Column(
                         children: <Widget>[
-                          TextField(
-                            maxLines: 1,
-                            decoration: InputDecoration(
-                                icon: Image.asset(
-                                  "images/2.0x/ic_2x_login_user.png",
-                                  width: 18,
-                                  height: 18,
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                TextField(
+                                  maxLines: 1,
+                                  decoration: InputDecoration(
+                                      icon: Image.asset(
+                                        "images/2.0x/ic_2x_login_user.png",
+                                        width: 18,
+                                        height: 18,
+                                      ),
+                                      border: InputBorder.none,
+                                      hintText: "请输入账号"),
+                                  controller: TextEditingController.fromValue(
+                                      TextEditingValue(
+                                          text:
+                                              '${this._userName == null ? "" : this._userName}',
+                                          selection: TextSelection.fromPosition(
+                                              TextPosition(
+                                                  affinity:
+                                                      TextAffinity.downstream,
+                                                  offset: '${this._userName}'
+                                                      .length)))),
+                                  onChanged: (text) {
+                                    this.setState(() {
+                                      _userName = text;
+                                    });
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  textInputAction: TextInputAction.done,
                                 ),
-                                border: InputBorder.none,
-                                hintText: "请输入账号"),
-                            controller: TextEditingController.fromValue(
-                                TextEditingValue(
-                                    text:
-                                        '${this._userName == null ? "" : this._userName}',
-                                    selection: TextSelection.fromPosition(
-                                        TextPosition(
-                                            affinity: TextAffinity.downstream,
-                                            offset:
-                                                '${this._userName}'.length)))),
-                            onChanged: (text) {
-                              this.setState(() {
-                                _userName = text;
-                              });
-                            },
-                            keyboardType: TextInputType.number,
-                            textInputAction: TextInputAction.done,
-                          ),
-                          TextField(
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            decoration: InputDecoration(
-                                icon: Image.asset(
-                                  "images/2.0x/ic_2x_login_password.png",
-                                  width: 18,
-                                  height: 18,
+                                TextField(
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  decoration: InputDecoration(
+                                      icon: Image.asset(
+                                        "images/2.0x/ic_2x_login_password.png",
+                                        width: 18,
+                                        height: 18,
+                                      ),
+                                      border: InputBorder.none,
+                                      hintText: "请输入密码"),
+                                  controller: TextEditingController.fromValue(
+                                      TextEditingValue(
+                                          text:
+                                              '${this._password == null ? "" : this._password}',
+                                          selection: TextSelection.fromPosition(
+                                              TextPosition(
+                                                  affinity:
+                                                      TextAffinity.downstream,
+                                                  offset: '${this._password}'
+                                                      .length)))),
+                                  onChanged: (text) {
+                                    this.setState(() {
+                                      this._password = text;
+                                    });
+                                  },
+                                  keyboardType: TextInputType.number,
+                                  textInputAction: TextInputAction.done,
                                 ),
-                                border: InputBorder.none,
-                                hintText: "请输入密码"),
-                            controller: TextEditingController.fromValue(
-                                TextEditingValue(
-                                    text:
-                                        '${this._password == null ? "" : this._password}',
-                                    selection: TextSelection.fromPosition(
-                                        TextPosition(
-                                            affinity: TextAffinity.downstream,
-                                            offset:
-                                                '${this._password}'.length)))),
-                            onChanged: (text) {
-                              this.setState(() {
-                                this._password = text;
-                              });
-                            },
-                            keyboardType: TextInputType.number,
-                            textInputAction: TextInputAction.done,
-                          ),
-                          Offstage(
-                            offstage: false,
-                            child: Text(
-                              '帐号密码错误',
-                              style: TextStyle(color: Colors.red),
+                                Offstage(
+                                    offstage: _isSuccess,
+                                    child: Container(
+                                      margin: EdgeInsets.only(left: 30),
+                                      child: Text(
+                                        '帐号密码错误',
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    )),
+                              ],
                             ),
                           ),
                         ],
@@ -109,15 +126,18 @@ class _LoginPageState extends State<LoginPage> {
                   width: double.infinity,
                   child: RaisedButton(
                       elevation: 2.0,
-                      color: Colors.blue,
+                      color: Colors.green,
                       textColor: Colors.white,
                       child: Text(
                         "登录",
                         style: TextStyle(fontSize: 20),
                       ),
                       onPressed: () async =>
-                          AppNavigator.toPushReplacementNamed(
-                              context, AppRoute.scanPage)),
+//                          AppNavigator.toPushReplacementNamed(
+//                              context, AppRoute.scanPage)
+                          setState(() {
+                            _isSuccess = false;
+                          })),
                 )
               ],
             ),
@@ -125,6 +145,12 @@ class _LoginPageState extends State<LoginPage> {
         ],
       )),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    FocusScope.of(context).requestFocus(FocusNode());
   }
 }
 
